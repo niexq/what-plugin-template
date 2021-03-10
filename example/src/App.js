93beac57aa876ed0d36048ca8afed6e98ed8741a
niexq <niexq@firstgrid.cn>
2021-03-10 18:05:08 +0800

    again add example

---
+++ example/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import { Button } from 'what-plugin-template';
+
+function App() {
+  return (
+    <div className="App">
+      <Button>确定</Button>
+    </div>
+  );
+}
+
+export default App;
